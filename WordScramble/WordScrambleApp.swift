568ed6e8e13fb8c65d51ab59a172d1586b88d49b
Anton Serdyuchenko <anton415@gmail.com>
2024-12-19 09:45:55 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Anton Serdyuchenko on 19.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
